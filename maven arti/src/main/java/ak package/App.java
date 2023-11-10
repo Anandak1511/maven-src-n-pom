77359db07677207e2a9b3a02b07c2bde62288e46
Anand <anand15141@gmail.com>
2023-11-10 14:54:06 +0530

    add files

---
+++ maven arti/src/main/java/ak package/App.java
@@ -0,0 +1,13 @@
+package ak package;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
